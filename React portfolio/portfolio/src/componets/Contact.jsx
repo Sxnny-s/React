47a3c516fbfa9859483667a4e6e2bfc4a800c765
Sxnny-s <ethsok@protonmail.com>
2025-02-10 17:40:41 -0500

    adding style to contact form

--- React portfolio/portfolio/src/componets/Contact.jsx
+++ React portfolio/portfolio/src/componets/Contact.jsx
@@ -1,15 +1,16 @@
 
 import React from 'react'
+import './contact.css'
 
 const Contact = () => {
   return (
     <div>
         <h2>Contact me here</h2>
-        <p>Let's connect! Feel free to reach out for collaborations or questions.</p>
+        
         <form  className="contact-form">
           <input type="text" placeholder="Your Name" required />
           <input type="email" placeholder="Your Email" required />
-          <textarea placeholder="Your Message" rows="8" required></textarea>
+          <textarea  placeholder="Your Message" rows="8" required></textarea>
           <button type="submit">Send</button>
         </form>
     </div>
